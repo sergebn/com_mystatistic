ba66e6db51d0eea93ed89d5ba95d95324f764739
sergebn <sergebn@mail.ru>
2015-11-19 01:15:05 +0200

    version 07

---
+++ admin/controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_myStatistic
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * General Controller of MyStatistic component
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_mystatistic
+ * @since       0.0.7
+ */
+class MyStatisticController extends JControllerLegacy
+{
+	/**
+	 * The default view for the display method.
+	 *
+	 * @var string
+	 * @since 12.2
+	 */
+	protected $default_view = 'mystatistics';
+}
